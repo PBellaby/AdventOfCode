4c9704e3287482a953b6aaab1c38967dced8c2c9
Tamillis <pbellaby@spartaglobal.com>
2022-12-18 21:53:30 +0000

    started day 7

--- 2022 Day 7/AdventOfCodeDay7/AdventOfCodeDay7/Program.cs
+++ 2022 Day 7/AdventOfCodeDay7/AdventOfCodeDay7/Program.cs
@@ -1,10 +1,92 @@
-﻿namespace AdventOfCodeDay7
+﻿namespace AdventOfCodeDay7;
+
+public class FileSystemItem
 {
-    internal class Program
+    public readonly string filepath;  //filepath from root to the dir containing this item (may just be root)
+    public readonly string name;
+    public List<FileSystemItem> contents; //file system items contained within
+    public bool isDir = false;
+    public int size = 0;
+
+    public FileSystemItem(string fp, string name)
     {
-        static void Main(string[] args)
+        filepath = fp;
+        this.name = name;
+        contents = new();
+    }
+
+    public override string ToString()
+    {
+        string output = $"{filepath}{name}\n\t";
+        foreach(var content in contents)
         {
-            string[] data = File.ReadAllLines(@"C:\Users\bella\Documents\SpartaGlobal\Advent of Code\2022 Day 7");
+            output += content.ToString();
+        }
+        return output;
+    }
+
+    public bool Exists (string name)
+    {
+        if (name == this.name) return true;
+        foreach(var content in contents)
+        {
+            if (content.Exists(name)) return true;
+        }
+        return false;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] data = File.ReadAllLines(@"C:\Users\bella\Documents\SpartaGlobal\Advent of Code\2022 Day 7\input.txt");
+
+        // measure the size of directories by all the files contained within (inc. contained directories)
+        // sum up the sizes of all directories that are at most 100_000 in size
+
+        string curDir = @"/";
+        string prevDir = curDir;
+
+        FileSystemItem fs = new(curDir, "");
+
+        Console.WriteLine(fs);
+
+        //process commands to build filesystem (fs)
+        foreach(var line in data)
+        {
+            //change directory
+            if(line.Contains("$ cd"))
+            {
+                string newDir = line.Remove(0, 5);
+                //swap prev and current directory if "$ cd .."
+                if (newDir == "..") (prevDir, curDir) = (curDir, prevDir);
+                else if (newDir == "/")
+                {
+                    prevDir = curDir;
+                    curDir = "/";
+                }
+                else curDir = curDir + "/" + newDir;
+            }
+            // "$ ls" can be ignored, file does this for us essentially
+
+            else if(line.ToLower().StartsWith("dir"))
+            {
+                //add directory to filesystem
+                string name = line.Remove(0, 4);
+                FileSystemItem newDir = new(curDir, name);
+                newDir.isDir = true;
+
+                //find current directory using curDir string, add new item to that particular FileSystemItem
+                // TODO
+            }
+
+            else
+            {
+                //add new file to fs
+
+                // TODO
+            }
         }
     }
 }
