8d40b141916cabc80205d3484eaf67e01df791f4
Tamillis <pbellaby@spartaglobal.com>
2022-12-18 11:11:27 +0000

    starting day 7

---
+++ 2022 Day 7/AdventOfCodeDay7/AdventOfCodeDay7/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCodeDay7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] data = File.ReadAllLines(@"C:\Users\bella\Documents\SpartaGlobal\Advent of Code\2022 Day 7");
+        }
+    }
+}
